6d42ddb340230a4bf1dc6cd53352d4c0fe659cdc
mguo80 <mguo80@yahoo.com>
2014-10-29 16:16:20 -0700

    Create MingCloud.h

---
+++ MingCloud.h
@@ -0,0 +1,22 @@
+//
+//  MingCloud.h
+//
+//  Created by Guo, Ming on 10/22/14.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol MingCloud
+
+@property (nonatomic, strong)   NSString *cloudName;
+
+-(id)getObjectByKey:(NSString*)key;
+
+/**
+ * @param obj - NSData, NSString, NSNumber, NSDate, NSArray, or NSDictionary
+ * @param key - string only
+ * @return YES if succeed, NO if fail
+ */
+-(BOOL)setObject:(id)obj ByKey:(NSString*)key;
+
+@end
