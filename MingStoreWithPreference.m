bb68a3288c2fc91b90b93ffe8a951d591aa787c1
mguo80 <mguo80@yahoo.com>
2014-10-29 16:13:57 -0700

    Create MingStoreWithPreference.m

---
+++ MingStoreWithPreference.m
@@ -0,0 +1,54 @@
+//
+//  MingStoreWithPreference.m
+//
+//  Created by Guo, Ming on 10/9/14.
+//
+
+#import "MingStoreWithPreference.h"
+
+@implementation MingStoreWithPreference
+
+@synthesize cloud;
+
+-(instancetype)initWithCloud:(id<MingCloud>)cld
+{
+    if (self = [super init])
+    {
+        self.cloud = cld;
+    }
+    return self;
+}
+
+-(id)getObjectByKey:(NSString*)key
+{
+    id obj = [[NSUserDefaults standardUserDefaults] objectForKey:key];
+    if (!obj)
+    {
+        obj = [self.cloud getObjectByKey:key];
+        if (obj)
+        {
+            [[NSUserDefaults standardUserDefaults] setObject:obj forKey:key];
+        }
+    }
+    return obj;
+}
+
+-(BOOL)setObject:(id)obj ByKey:(NSString*)key
+{
+    [[NSUserDefaults standardUserDefaults] setObject:obj forKey:key];
+    return [self.cloud setObject:obj ByKey:key];
+}
+
+-(id)getCustomObjectByKey:(NSString *)key
+{
+    NSData *data = [self getObjectByKey:key];
+    return [NSKeyedUnarchiver unarchiveObjectWithData:data];
+}
+
+-(BOOL)setCustomObject:(id<NSCoding>)obj ByKey:(NSString *)key
+{
+    NSData *data = [NSKeyedArchiver archivedDataWithRootObject:obj];
+    return [self setObject:data ByKey:key];
+}
+
+@end
