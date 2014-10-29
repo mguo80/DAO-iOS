f15c6f2978e8e3af8a908ce8d5b85b88c978963d
mguo80 <mguo80@yahoo.com>
2014-10-29 16:14:54 -0700

    Create MingStoreWithFile.m

---
+++ MingStoreWithFile.m
@@ -0,0 +1,69 @@
+//
+//  MingStoreWithFile.m
+//
+//  Created by Guo, Ming on 9/12/14.
+//
+
+#import "MingStoreWithFile.h"
+#import <TMCache/TMCache.h>
+
+@interface MingStoreWithFile()
+@property (nonatomic, strong)   TMDiskCache* cache;
+@end
+
+@implementation MingStoreWithFile
+
+@synthesize cloud;
+
+-(instancetype)initWithCloud:(id<MingCloud>)cld
+{
+    if (self = [super init])
+    {
+        self.cache = [TMDiskCache sharedCache];
+        self.cloud = cld;
+    }
+    return self;
+}
+
+-(instancetype)initWithName:(NSString *)name andCloud:(id<MingCloud>)cld
+{
+    if (self = [super init])
+    {
+        self.name = name;
+        self.cache = [[TMDiskCache alloc] initWithName:name];
+        self.cloud = cld;
+    }
+    return self;
+}
+
+-(id)getObjectByKey:(NSString *)key
+{
+    id obj = [self.cache objectForKey:key];
+    if (!obj)
+    {
+        obj = [self.cloud getObjectByKey:key];
+        if (obj)
+        {
+            [self.cloud setObject:obj ByKey:key];
+        }
+    }
+    return obj;
+}
+
+-(BOOL)setObject:(id)obj ByKey:(NSString *)key
+{
+    [self.cache setObject:obj forKey:key];
+    return [self.cloud setObject:obj ByKey:key];
+}
+
+-(id)getCustomObjectByKey:(NSString *)key
+{
+    return [self getObjectByKey:key];
+}
+
+-(BOOL)setCustomObject:(id<NSCoding>)obj ByKey:(NSString *)key
+{
+    return [self setObject:obj ByKey:key];
+}
+
+@end
